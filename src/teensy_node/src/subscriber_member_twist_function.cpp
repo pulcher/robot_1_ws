bcc208b348e77bcc999f70edbf8b26a70ebf505a
Harold Pulcher <pulcher@killercomputing.com>
2021-02-10 21:32:59 -0600

    got somethig working on receiveing the cmd_vel

--- src/teensy_node/src/subscriber_member_twist_function.cpp
+++ src/teensy_node/src/subscriber_member_twist_function.cpp
@@ -1,27 +1,28 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-using stdio::placeholders::_1;
+#include "geometry_msgs/msg/twist.hpp"
+using std::placeholders::_1;;
 
 class MinimalSubscriber : public rclcpp::Node
 {
     public:
-        MinimalSubscriber() : ("minimal_subscriber")
+        MinimalSubscriber()
+        : Node("minimal_subscriber")
         {
-            subscription_ = this->create_subscription<std_msgs::msg::String>( "topic", 10, 
-                std::bind(&MinialSubscriber::topic_callback, this, _1));
+            subscription_ = this->create_subscription<geometry_msgs::msg::Twist>( "cmd_vel", 10, 
+                std::bind(&MinimalSubscriber::topic_callback, this, _1));
         }
 
     private:
-        void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+        void topic_callback(const geometry_msgs::msg::Twist::SharedPtr msg) const
         {
-            RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+            RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->linear.x);
         }
-        rclcpp::Subscript<std_msgs::msg:String>::SharedPtr subscription_;
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
 };
 
-int main(int argc; char * argv{})
+int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MinimalSubscriber>());
